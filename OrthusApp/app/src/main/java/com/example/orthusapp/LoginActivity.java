3246b317e5e53a2798842eba7e992e5e1d445f7a
ChenChai <chen.chai@outlook.com>
2018-11-12 07:37:22 -0500

    Implemented login functionality

--- OrthusApp/app/src/main/java/com/example/orthusapp/LoginActivity.java
+++ OrthusApp/app/src/main/java/com/example/orthusapp/LoginActivity.java
@@ -1,20 +1,29 @@
 package com.example.orthusapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emaiLEditText;
+    EditText passwordEditText;
+
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // TODO: Let user login with FirebaseAuth
 
     }
 
@@ -22,10 +31,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         // if the user is already signed in, they don't need the login screen
-        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+        if(mFirebaseAuth.getCurrentUser() != null){
             finish();
         }
+
+        emaiLEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+
 
     }
 
@@ -35,7 +51,20 @@ public class LoginActivity extends AppCompatActivity {
 
     // called when login button is pressed.
     public void loginMe(View view){
-        // TODO: implement authentication
+        String email = emaiLEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
