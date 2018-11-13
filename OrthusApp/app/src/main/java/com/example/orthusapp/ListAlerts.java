190277b80ef5027c1f7449d48583fe9eccd2a8da
ChenChai <chen.chai@outlook.com>
2018-11-13 08:04:02 -0500

    Added java class for recycler

---
+++ OrthusApp/app/src/main/java/com/example/orthusapp/ListAlerts.java
@@ -0,0 +1,22 @@
+package com.example.orthusapp;
+
+import android.app.LauncherActivity;
+
+public class ListAlerts {
+
+    private String time;
+    private String armed;
+
+    public ListAlerts(String mTime, String mArmed){
+        mTime = time;
+        mArmed = armed;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getArmed() {
+        return armed;
+    }
+}
