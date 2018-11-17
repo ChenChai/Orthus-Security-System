3082ed584d460f8f5f824e205c4b3ceb14902956
Lukman Mohamed <l3mohame@edu.uwaterloo.ca>
2018-11-17 15:39:00 -0500

    The Calm Before the Storm

--- ESP8266_Firebase_Prototype/Arduino_Firebase_Connect/Arduino_Firebase_Connect.ino
+++ ESP8266_Firebase_Prototype/Arduino_Firebase_Connect/Arduino_Firebase_Connect.ino
@@ -1,46 +1,41 @@
+// This program will allow the ESP8266 to listen to input from the Arduino Mega and connect to wifi.
 
 #include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+/*
+const char *ssid = "HPENVY";
+const char *password = "wbnu5405";
+*/
 
-#include <ArduinoJson.h>
-#include <FirebaseArduino.h> // note: need to run ArduinoJson on 5.13.2 to work.
+int i = 0;
 
+void setup(){
+  WiFi.begin("HPENVY", "wbnu5405");
+  Serial.begin(115200);
 
-
-#include <SoftwareSerial.h> // serial command library, I think? see https://www.arduino.cc/en/Reference/SoftwareSerial for details
-
-#define RX 10 // connected to TX of ESP8266
-#define TX 11 // connected to RX of ESP8266
-
-
-SoftwareSerial esp8266(RX,TX); // creates instance of SoftwareSerial object; we'll name it esp8266
-// RX is the pin on which to recieve data
-// TX is the pin on which to send data; check the documentation of specific arduino to see if it works
-
-
-#define WIFI_SSID "HPENVY" // test hotspot, change to personal wifi
-#define WIFI_PASSWORD "wbnu5405"
-
-// we'll just have this here to ensure the Arduino Mega can still do stuff
-int ledPin = 13;
-
-void setup() {
-  pinMode(ledPin, OUTPUT); 
-
-  // TODO: figure out exactly what this code does
-  Serial.begin(9600); // set baud rate of Arduino Mega
-  esp8266.begin(115200); // set baud rate of esp8266 
+  Serial.print(WiFi.localIP());
+  // get the secret from account settings > service accounts > database secrets in Firebase Console.
+  Firebase.begin("fir-vertical-prototype.firebaseio.com","MbcYHrAArRq9WhAX4VXOm6UDupZ3Qz39yIkfAyTS");
   
-  // transmit data to the esp8266 command line
-  esp8266.println("AT");
-  esp8266.println("AT+CWMODE=1"); // sets ESP8266 to station mode, instead of AP mode.
-  esp8266.println("AT+CWJAP=\"HPENVY\",\"wbnu5405\""); // need the escape characters as the command line needs quotes in the command.
-}
-
-void loop() {
+} 
+void loop(){
   
-  digitalWrite(ledPin,HIGH);
-  delay(2000);
-  digitalWrite(ledPin, LOW);
-  delay(2000);
+  Firebase.setInt("testValue", 1);
+  if(Firebase.failed()){
 
+    Serial.println(Firebase.error());
+  } else{
+    Serial.println("Set 1 succeeded!");
+  }
+  delay(1000);
+
+  Firebase.setInt("testValue", 0);
+  if(Firebase.failed()){
+
+    Serial.println(Firebase.error());
+  } else{
+    Serial.println("Set 0 succeeded!");
+  }
+  delay(1000);
 }
+ 
